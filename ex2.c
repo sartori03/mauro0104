1caa47559cec0d327639b2fb7aaab837f7780b5e
sartori03 <marcolinosartori03@gmail.com>
2025-04-08 20:01:32 -0300

    ex2.c

--- ex2.c
+++ ex2.c
@@ -3,9 +3,13 @@
 int main(void) {
     int num, soma = 0;
  do {
-        printf("Digite um número: ");
+        printf("Digite um número [1 - 6]: ");
         scanf("%d", &num);
-        
+
+        if(num < 1 || num > 6) {
+            printf("Apenas números de 1 a 6!\n");
+                continue;
+            }
         if(num % 2 == 0) {
             soma += num;
         } else {
